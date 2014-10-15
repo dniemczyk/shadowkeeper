a91f8489bad21409abbaf103617d945524932253
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-15 22:23:37 +0200

    Remove some unneeded whitespace

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page" do
-
     it "should have the right title" do
       visit '/static_pages/home'
       expect(page).to have_title('ShadowKeeper | Welcome')
@@ -16,7 +15,6 @@ describe "StaticPages" do
   end
 
   describe "Help Page" do
-
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -24,7 +22,6 @@ describe "StaticPages" do
   end
 
   describe "About page" do
-
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
