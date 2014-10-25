9a9949d6482a6b93c1bb86ca86097d85c65bd165
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-26 00:35:08 +0200

    Users e-mail should be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true, length: { maximum: 50 }
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  validates :name,
+    presence: true,
+    length: { maximum: 50 }
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: VALID_EMAIL_REGEX }
 end
