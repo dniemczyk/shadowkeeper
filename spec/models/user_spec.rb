139ef44f488ae79e838d0adba07ef77ce8033393
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-27 17:44:30 +0100

    Include a secure pass to user - first try
    Wrote several tests for the secure password, and made them
    pass using the 'has_secure_password' method on User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User",
+                     email: "user@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -18,6 +25,19 @@ describe User do
 
   describe "when email not present" do
     before { @user.email = " " } # Remove the email for the 'Example User'
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user.password = " "
+      @user.password_confirmation = " "
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "plain_wrong" }
     it { should_not be_valid }
   end
 
