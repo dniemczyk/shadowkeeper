31c8246d749f2adb12e7ea067fa42267153e44c5
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-10-29 18:35:14 +0100

    Add a title and heading for the User profile page

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    email "MyString"
+    name     "Damian Niemczyk"
+    email    "dn@example.com"
+    password "secretfoo"
+    password_confirmation "secretfoo"
   end
 end
