5644f1aab997e753ad4c3d47941e7b182603010a
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-12-07 17:27:09 +0100

    Handle signup with invalid email/password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # Sign in user and redirect to user page
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def delete
