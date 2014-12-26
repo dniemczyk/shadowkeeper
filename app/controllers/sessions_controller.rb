35e6c3a8f763c311eda8ceaf9c37727736bd5b4a
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-12-26 13:13:18 +0100

    Add a signin, signout layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      # Sign in user and redirect to user page
+      sign_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
