0f639d9bd50751b2a33e1d64e502b9155d40279a
Damian Niemczyk <djniemczyk7@googlemail.com>
2014-12-27 20:38:37 +0100

    Add a sign out functionality
    The permament branch is still not working as expected.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,15 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  # Signs out the current user
+  def sign_out
+    session.delete(:user_id)
+    cookies.delete(:user_id)
+    self.current_user = nil
+  end
 end
