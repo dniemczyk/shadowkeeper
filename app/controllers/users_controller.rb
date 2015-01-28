3e202ac781cc156d69181959b144aae530dcd196
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-01-28 21:58:10 +0100

    Add a user delete action and links
    The delete action should only be viewable for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 
   def index
@@ -34,6 +34,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find_by(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_path
   end
 
   private
